d1f3f6560da2af5047cf3171c156564ac30b8586
JarDar <jardar.tsai@gmail.com>
2022-09-08 15:05:04 +0800

    效果，圖片ratio

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,85 +1,111 @@
-$(function () {
-  console.log('Hello Bootstrap5');
+// $(function () {
+console.log('Hello Bootstrap5');
+
+const phoneSearch = document.querySelector("#toggleSearch");
+// phone 版 search bar
+const searchBack = document.querySelector("#searchBack");
+const searchBtn = document.querySelector("#searchBtn");
+const headerSearchBar = document.querySelector("#headerSearchBar");
 
 
-  const swiper = new Swiper('.swiper-container', {
-    centeredSlides: true,
-    loop: true,
-    speed: 500,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    autoplay: {
-      delay: 3000,
+phoneSearch.addEventListener("click", (e) => {
+  e.stopPropagation()
+  // headerSearchBar.classList.toggle("d-none")
+  headerSearchBar.classList.remove("d-none")
+
+})
+searchBack.addEventListener("click", (e) => {
+  // e.stopPropagation()
+  e.preventDefault();
+  headerSearchBar.classList.add("d-none")
+})
+searchBtn.addEventListener("click", (e) => {
+  // e.stopPropagation()
+  e.preventDefault();
+  headerSearchBar.classList.add("d-none")
+
+  location.href = "art-filter.html"
+})
+
+
+const swiper = new Swiper('.swiper-container', {
+  centeredSlides: true,
+  loop: true,
+  speed: 500,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  breakpoints: {
+
+    640: {
+      slidesPerView: 1,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    768: {
+      slidesPerView: 1,
     },
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-    breakpoints: {
-
-      640: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 1,
-      },
-      992: {
-        slidesPerView: 1.75,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
+    992: {
+      slidesPerView: 1.75,
     },
-  });
-
-  const swiperArt = new Swiper('.swiper-art-container', {
-    centeredSlides: true,
-    loop: true,
-    speed: 500,
-    slidesPerView: 2.25,
-    spaceBetween: 24,
-    autoplay: {
-      delay: 3000,
+    1024: {
+      slidesPerView: 3,
     },
-    pagination: {
-      el: ".swiper-art-pagination",
-      clickable: true,
+  },
+});
+
+const swiperArt = new Swiper('.swiper-art-container', {
+  centeredSlides: true,
+  loop: true,
+  speed: 500,
+  slidesPerView: 2.25,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-art-pagination",
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+  breakpoints: {
+
+    640: {
+      slidesPerView: 2.25,
     },
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
-    breakpoints: {
-
-      640: {
-        slidesPerView: 2.25,
-      },
-      768: {
-        slidesPerView: 2.25,
-      },
-      992: {
-        slidesPerView: 4.25,
-      },
-      1024: {
-        slidesPerView: 4.25,
-      },
+    768: {
+      slidesPerView: 2.25,
     },
-  });
+    992: {
+      slidesPerView: 4.25,
+    },
+    1024: {
+      slidesPerView: 4.25,
+    },
+  },
+});
 
 
-  // // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
-  // $('.masonry-row').imagesLoaded().progress(function () {
-  //   $('.masonry-row').masonry(); // 渲染整體畫面
-  // });
+// // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
+// $('.masonry-row').imagesLoaded().progress(function () {
+//   $('.masonry-row').masonry(); // 渲染整體畫面
+// });
 
-  $('.masonryTab').on('shown.bs.tab', function () {
-    let msnry = Masonry.data($('.masonry-row')[0]);
-    $('.masonry-row').imagesLoaded(function () {
-      msnry.layout(); // 渲染整體畫面
-    });
+$('.masonryTab').on('shown.bs.tab', function () {
+  let msnry = Masonry.data($('.masonry-row')[0]);
+  $('.masonry-row').imagesLoaded(function () {
+    msnry.layout(); // 渲染整體畫面
   });
 });
+// });
